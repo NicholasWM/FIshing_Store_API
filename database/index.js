2ba2434e907cd5eaf5fc6cbc36823b1dd672abbe
NicholasWM <nicholaswmazzei@gmail.com>
2019-11-11 21:11:17 -0300

    fix: add model estoque

--- database/index.js
+++ database/index.js
@@ -6,6 +6,7 @@ const LivroCaixa = require('../app/models/LivroCaixa');
 const Produtos = require('../app/models/Produtos');
 const User = require('../app/models/User');
 const Compras_Produtos = require('../app/models/Compras_Produtos');
+const Estoque = require('../app/models/Estoque');
 
 const connection = new Sequelize(dbConfig);
 
@@ -14,9 +15,11 @@ Compras.init(connection);
 Produtos.init(connection);
 LivroCaixa.init(connection);
 Compras_Produtos.init(connection);
+Estoque.init(connection);
 
 Compras.associate(connection.models);
 Produtos.associate(connection.models);
 Compras_Produtos.associate(connection.models);
+Estoque.associate(connection.models);
 
 module.exports = connection;
