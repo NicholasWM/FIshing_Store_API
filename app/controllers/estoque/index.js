ca9d54d9e5a6962a38ad92943207f84d72421c9a
NicholasWM <nicholaswmazzei@gmail.com>
2019-11-13 16:33:49 -0300

    feat: Controllers basicos do Estoque

--- app/controllers/estoque/index.js
+++ app/controllers/estoque/index.js
@@ -1,4 +1,5 @@
 const Estoque = require('../../models/Estoque');
+const Produtos = require('../../models/Produtos');
 const {
     mes_atual,
     mes_passado,
@@ -18,5 +19,40 @@ module.exports = {
         const estoque= await Estoque.findAll()
         res.json(estoque)
     },
+    inserir_registro: async(req, res)=>{
+        if(!!!req.body){
+            return res.status(400).send({'msg':""})
+        }
+        const {produto_id, modo, quantidade} = req.body
+
+        const produto = await Produtos.findByPk(produto_id)
+        if(!produto){
+            return res.status(400).send({"msg": "Produto não existe"})
+        }
+
+        if (modo.toLowerCase() != "entrada" && modo.toLowerCase() != "saida") {
+            return res.status(400).send({'msg': "modo inválido!"})
+        }
+
+        modo.toLowerCase() == "entrada" ? adiciona_ao_estoque(produto, quantidade, req.body.custo) : retira_do_estoque(produto, quantidade)
+
+        return res.json(produto)
+
+        async function adiciona_ao_estoque(produto, quantidade, custo) {
+            if(!custo){
+                return res.json({'msg': "Custo inexistente"})
+            }
+            await produto.update({ quantidade: produto.quantidade + quantidade })
+            await Estoque.create({ produto_id, modo, preco: custo, quantidade })
+        }
+        async function retira_do_estoque(produto, quantidade) {
+            if (quantidade > produto.quantidade) {
+                return res.status(400).send({ 'msg': "Itens insuficientes no estoque" })
+            }
+            await produto.update({ quantidade: produto.quantidade - quantidade })
+            await Estoque.create({ produto_id, modo, preco: produto.preco * quantidade, quantidade })
+        }
+
+    }
     
 }
