0cfc5871e43318f69ccbead3ae46f9bb78954ede
NicholasWM <nicholaswmazzei@gmail.com>
2019-11-14 21:48:10 -0300

    BD: Ligacao entre Estoque e Compras_Produtos

--- app/models/Estoque.js
+++ app/models/Estoque.js
@@ -13,6 +13,11 @@ class Estoque extends Model {
     }
     static associate(models) {
         this.belongsTo(models.Produtos, { foreignKey: 'produto_id', as: 'produto' });
+
+        // this.belongsTo(models.Compras_Produtos, { foreignKey: 'compra_produto_id', as: 'compra_produto' });
+
+        // this.hasOne(models.Compras_Produtos, { foreignKey: 'compra_produto_id', targetKey: 'id' });
+
     }
 }
 
