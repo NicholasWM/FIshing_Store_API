e7d36bac03350c8eb52ae3e4541ee0782a5c9ce9
NicholasWM <nicholaswmazzei@gmail.com>
2019-11-23 07:57:06 -0300

    add: Adaptando o projeto para trabalhar com socketio

--- index.js
+++ index.js
@@ -2,9 +2,32 @@ const express = require('express');
 const cors = require('cors')
 const routes = require('./routes')
 const path = require('path')
+const socketio = require('socket.io');
+const http = require('http');
+
+const app = express();
+
+const server = http.Server(app);
+const io = socketio(server);
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+    console.log(user_id);
+    
+    connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+    
+
+    return next();
+})
+
 
 require('./database');
-const app = express();
 app.use(cors()) // Estudar CORS, ele permite que se controle quem tem acesso a API
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: false }));
@@ -12,4 +35,4 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/files', express.static(path.resolve(__dirname, 'uploads')))// Forma q o Express utiliza para retornar arquivos staticos(img,pdfs...)
 
 app.use(routes)
-app.listen(3333);
+server.listen(3333);
