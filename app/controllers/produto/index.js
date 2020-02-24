b81d10ceb57e6642fe7a75ebe5ac3ca07011276b
Nicholas W Mazzei <nicholaswesleymazzei@gmail.com>
2020-02-24 12:29:33 -0300

    feat: Contagem de itens diferentes, unidades e valor em estoque

--- app/controllers/produto/index.js
+++ app/controllers/produto/index.js
@@ -24,18 +24,31 @@ module.exports = {
         return res.json(busca)
     },
     list_categorias: async (req, res)=>{
+        const quantidade_unicos = 0
+        const quantidade_estoque = 0
         const produtos =  await Produtos.findAll({
             attributes:['id','nome', 'quantidade', 'categoria', "imagem", "preco"]
         })
         let categorias = []
         produtos.filter(produto => {
             if(!categorias.filter(categoria => {
+                //Verifica se a categoria já está registrada
                 if (produto.categoria == categoria.categoria){
+                    //Atualiza a categoria com o novo produto
                     categoria.produtos.push(produto)
+                    categoria.itens_diferentes+=1
+                    categoria.total_unidades+=produto.quantidade
+                    categoria.valor_em_estoque+=produto.quantidade*produto.preco
                 }
                 return produto.categoria == categoria.categoria
             }).length){
-                return categorias.push({categoria:produto.categoria, produtos:[produto]})
+                return categorias.push({
+                    categoria:produto.categoria,
+                    produtos:[produto],
+                    valor_em_estoque:produto.quantidade*produto.preco,
+                    itens_diferentes:1,
+                    total_unidades:produto.quantidade
+                })
             }
         })
         res.json(categorias)
